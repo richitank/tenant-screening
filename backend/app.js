0d6662e1c96252883ac3146d45b2305e7c20ff21
amshu-k <k.amshuman@gmail.com>
2018-06-27 03:29:38 -0400

    Changed Readme.md

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,10 @@ const checkAuth = require('../backend/middleware/check-auth')
 
 const app = express();
 
+const screeningRoutes = require("./routes/screeningRequestForms")
 const userRoutes = require("./routes/user");
+
+
 
 //const paymentRoutes = require("./payment/paypal");
 
@@ -39,92 +42,7 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('/api/screeningInfo', (req, res, next) => {
-
-    if(req.body.screeningCost == 1) {
-        req.body.screeningCost = 39.99
-    }
-    else if(req.body.screeningCost == 2 || req.body.screeningCost == 3) {
-        req.body.screeningCost = 24.99
-        console.log(req.body.screeningCost) }
-
-    const output = `<h3>
-    <p>Hi, <br> There is a request for you to get a background screening done.  <br>
-    <br> You'll be paying ${req.body.screeningCost} for the screening.<br>
-    <p>Please go to the following link to proceed: <a href="http://localhost:4201">Get Started</a></p>
-    <br>
-    <p>Have questions? Please contact <a href="http://localhost:4202"> Customer Support</a></p>`;
-
-    // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: 'amshu.k1956@gmail.com', // generated ethereal user
-        pass: 'qazwsx6969'  // generated ethereal password
-    },
-    tls:{
-      rejectUnauthorized:false
-    }
-  });
-
-  // setup email data with unicode symbols
-  let mailOptions = {
-      from: '"OffrBox" <amshu.k1956@gmail.com>', // sender address
-      to: req.body.applicantEmail, // list of receivers
-      subject: 'OffrBox - Background Screening Required', // Subject line
-      text: 'Hello world?', // plain text body
-      html: output // html body
-  };
-
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);   
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-      res.render('contact', {msg:'Email has been sent'});
-  });
-
-
-  //Inserting data to DB
-  const signupForm = new ScreeningRequestForm({
-    applicantFirstName: req.body.applicantFirstName,
-    applicantLastName: req.body.applicantLastName,
-    applicantEmail: req.body.applicantEmail,
-    applicantPhoneNo: req.body.applicantPhoneNo,
-    screeningCost: req.body.screeningCost,
-    //creator: req.u
-  });
-//console.log(req.userData)
-//return res.status(200).json({})
-  signupForm.save();
-
-  res.status(201).json({
-     message: 'Email received at server succesfully' 
-  });
-  });
-  
-
-//Get/Fetch Data from DB for the Owner
-  app.get('/api/welcome', checkAuth, (req, res, next) => {
-    ScreeningRequestForm.find()
-        .then(documents => {
-            res.status(200).json({
-                ScreeningRequestForms: documents
-            });
-        })
-        .catch((error) => {
-            console.log("Error:" + error)
-        })
-
-});
-
-
-//Get/Fetch Data from DB for the TenantView
+//Get/Fetch Data from DB for the TenantView. Called from 
 app.get('/api/getInfo', (req, res, next) => {
     ScreeningRequestForm.find()
         .then(documents => {
@@ -138,13 +56,6 @@ app.get('/api/getInfo', (req, res, next) => {
 
 });
 
-// app.get("/api/payment/paypal", (req, res, next)=> {
-//     //const post = req.body;
-//     //console.log(post);
-//     res.status(201).json({
-//         message: "testing"
-//     });
-// });
 
 app.post("/api/payment/paypal", (req, res, next)=> { 
     console.log(req.body)
@@ -153,6 +64,7 @@ app.post("/api/payment/paypal", (req, res, next)=> {
     })
 });
 
+app.use("/api/screeningInfo", screeningRoutes)
 app.use("/api/user", userRoutes);
 //app.use("/api/payment/paypal", paymentRoutes);
 
