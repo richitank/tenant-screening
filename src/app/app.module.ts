a07a9a3d7286bcc7ec7509ffc288e2c87a1f8ed3
amshu-k <k.amshuman@gmail.com>
2018-07-12 13:02:20 -0400

    Added step1 for the application form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
 import { ScreeningRequestComponent } from './screeningRequestForm/screeningRequest/screeningRequest.component';
@@ -77,6 +79,7 @@ const appRoutes = [
     FormsModule,
     HttpModule,
     RouterModule.forRoot(appRoutes),
+    NgbModule.forRoot(),
     HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule
