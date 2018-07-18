561fb9088240c7562e269e192f7922f93d1794c2
amshu-k <k.amshuman@gmail.com>
2018-07-18 15:11:56 -0400

    Add autoauth user to prevent logout by reloading

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -26,10 +26,10 @@ export class NavbarComponent implements OnInit, OnDestroy {
       });
 
     //Tenant auth listener
+    this.tenantIsAuthenticated = this.tenantAuth.getIsAuth();
     this.tenantAuthListenerSubs = this.tenantAuth.getAuthStatusListener()
       .subscribe(isAuthenticated => {
         this.tenantIsAuthenticated = isAuthenticated
-        console.log(this.tenantIsAuthenticated)
       });
   }
   ngOnDestroy() {
--- src/app/tenant/tenant-authentication/tenant-auth.service.ts
+++ src/app/tenant/tenant-authentication/tenant-auth.service.ts
@@ -36,11 +36,11 @@ export class TenantAuth {
                 this.token = token;
                 if (token) {
                     const expiresInDuration = response.expiresIn;
-                    this.tokenTimer = setTimeout(() => { this.logout() }, expiresInDuration * 1000)
+                    this.setAuthTimer(expiresInDuration);
                     this.isAuthenticated = true;
                     this.authStatusListener.next(true);
                     const now = new Date();
-                    const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+                    const expirationDate = new Date(now.getTime() + expiresInDuration * 500);
                     this.saveAuthData(token, expirationDate);
                     this.router.navigate(['/tenant-dashboard']);
                 }
@@ -78,5 +78,34 @@ export class TenantAuth {
     deleteAuthData() {
         localStorage.removeItem('token');
         localStorage.removeItem('expiration');
+    }
+
+    autoAuthUser() {
+        const authInformation = this.getAuthData();
+        const now = new Date();
+        const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+        if (expiresIn > 0) {
+            this.token = authInformation.token;
+            this.isAuthenticated = true;
+            this.setAuthTimer(expiresIn);
+            this.authStatusListener.next(true);
+        }
+    }
+
+    private setAuthTimer(expiresInDuration: number) {
+        this.tokenTimer = setTimeout(() => { this.logout() }, expiresInDuration * 500)
+    }
+
+    private getAuthData() {
+        const token = localStorage.getItem('token');
+        const expirationDate = localStorage.getItem('expiration');
+        if (!token || !expirationDate) {
+            return;
+        }
+        return {
+            token: token,
+            expirationDate: new Date(expirationDate)
+
+        }
     }
 }
